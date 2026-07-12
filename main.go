e7c2c0ff09be9a07cc56b8adc9a595e3bc56986a
Amber Welby <amber.welby@gmail.com>
2026-07-12 17:36:53 -0400

    Create bot session, parse birthdays.json

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
-	"fmt"
+	"os/signal"
+	"syscall"
+
+	"encoding/json"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
@@ -14,5 +21,65 @@ func main() {
 	}
 
 	token := string(tokenFile)
-	fmt.Println(token)
+
+	// Handle json file
+	birthdayFile, err := os.Open("birthdays.json")
+
+	if err != nil {
+		fmt.Println("Read error")
+		log.Fatal(err)
+	}
+
+	defer birthdayFile.Close()
+
+	/// Unmarshalling JSON
+	byteValue, err := io.ReadAll(birthdayFile)
+	if err != nil {
+		fmt.Println("Byte string error")
+		log.Fatal(err)
+	}
+
+	var months map[string]map[string][]string
+
+	err = json.Unmarshal(byteValue, &months)
+	if err != nil {
+		fmt.Println("Unmarshal Error")
+		log.Fatal(err)
+	}
+
+	// Create new session
+	sess, err := discordgo.New(fmt.Sprintf("Bot %s", token))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Handle recieved messages
+	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == s.State.User.ID {
+			return
+		}
+
+		if m.Content == "Birthdays?" {
+			names := months["January"]["01"]
+			for _, name := range names {
+				s.ChannelMessageSend(m.ChannelID, name)
+			}
+		}
+	})
+
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	// Open websocket
+	err = sess.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sess.Close()
+
+	fmt.Println("Birthday bot is online!")
+
+	// Handle closing
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
 }
